492ba5e7ea8ccc041b1547e3af95cbee080a80ce
Hoon Wee <mrgravity817@gmail.com>
2023-12-16 15:47:59 +0900

    feat: add daisyui

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :blurred, Blurred.Repo,
   database: "blurred_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  port: 5454
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
