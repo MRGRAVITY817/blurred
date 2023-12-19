890ab1ea6759a480187942e9a919ed5ba2fa7d16
Hoon Wee <mrgravity817@gmail.com>
2023-12-19 15:40:19 +0900

    feat: add selected attr in button

--- lib/blurred_web/components/buttons.ex
+++ lib/blurred_web/components/buttons.ex
@@ -4,12 +4,21 @@ defmodule BlurredWeb.Buttons do
 
   attr :icon, :string, required: true
   attr :title, :string, required: true
+  attr :selected, :boolean, default: false
 
   def friends_section_button(assigns) do
+    button_style =
+      "rounded-md pl-2 py-2 w-full flex flex-row items-center justify-start gap-2 " <>
+        if assigns.selected do
+          "bg-slate-200 text-black"
+        else
+          "bg-transparent hover:bg-slate-200 transition-all text-slate-500 hover:text-black"
+        end
+
+    assigns = assign(assigns, :button_style, button_style)
+
     ~H"""
-    <button class="bg-transparent hover:bg-slate-200 transition-all 
-                   rounded-md pl-2 py-2 w-full 
-                   flex flex-row items-center justify-start gap-2">
+    <button class={@button_style}>
       <.icon name={@icon} class="h-5 w-5" />
       <span class="font-medium text-sm">
         <%= @title %>
--- lib/blurred_web/components/sections/friends_section.ex
+++ lib/blurred_web/components/sections/friends_section.ex
@@ -1,6 +1,7 @@
 defmodule BlurredWeb.Sections.FriendsSection do
   import BlurredWeb.TextInput
   import BlurredWeb.Separator
+  import BlurredWeb.Buttons
   use Phoenix.Component
 
   def friends_section(assigns) do
@@ -13,15 +14,19 @@ defmodule BlurredWeb.Sections.FriendsSection do
       <div class="py-2 h-1 w-full">
         <.separator />
       </div>
-
+      <!-- Main button groups -->
       <div class="px-2 flex flex-col gap-2 w-full">
-        <BlurredWeb.Buttons.friends_section_button icon="hero-user-solid" title="Friends" />
-        <BlurredWeb.Buttons.friends_section_button icon="hero-fire-solid" title="Nitro" />
-        <BlurredWeb.Buttons.friends_section_button
-          icon="hero-envelope-solid"
-          title="Message Requests"
-        />
-        <BlurredWeb.Buttons.friends_section_button icon="hero-shopping-cart-solid" title="Shop" />
+        <.friends_section_button icon="hero-user-solid" title="Friends" selected={true} />
+        <.friends_section_button icon="hero-fire-solid" title="Nitro" />
+        <.friends_section_button icon="hero-envelope-solid" title="Message Requests" />
+        <.friends_section_button icon="hero-shopping-cart-solid" title="Shop" />
+      </div>
+      <!-- Friends list -->
+      <div class="px-2 mt-4 w-full">
+        <div class="flex flex-row items-center justify-between pl-1 pr-2">
+          <h2 class="text-slate-500 font-medium text-xs">DIRECT MESSAGES</h2>
+          <h2 class="text-slate-500 font-semibold text-sm">+</h2>
+        </div>
       </div>
     </div>
     """
